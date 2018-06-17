7a0d42441a81d057f838addb782e7fb05c21541b
mustafazc <mustafa@brndstr.com>
2018-06-17 11:38:55 +0400

    added youtube url functionality + slack replies

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const Router = require('./routes/routes.js')
+const bodyParser = require('body-parser')
 
 app.use(morgan((tokens, req, res) => {
   return `${tokens.method(req, res)} ${tokens.url(req, res)} ${tokens.status(req, res)} ${`${tokens['response-time'](req, res)}ms`} ${tokens.date(req, res)}`
 }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Router)
 
 const port = process.env.PORT || 8000
