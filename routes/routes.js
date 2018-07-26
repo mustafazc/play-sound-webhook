d67afbb6cf85a5ff8322f7867c2fdf931fb9052d
mustafazc <mustafa@brndstr.com>
2018-07-26 09:40:57 +0400

    Added endpoint for public ip

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const PlaySoundController = require('../controllers/PlaySoundController')
+const publicIp = require('public-ip')
 
 router.get('/health', (req, res) => res.status(200).send('OK'))
 router.post('/play', PlaySoundController.handle)
+router.get('/ip', (req, res) => publicIp.v4().then(ip => res.send(ip)))
 
 module.exports = router
