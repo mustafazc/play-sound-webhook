fc51c9004c7c26762763e0e30133abf4d2cb308e
mustafazc <mustafa@brndstr.com>
2019-01-10 07:46:11 +0400

    cleaned up controller

--- controllers/PlaySoundController.js
+++ controllers/PlaySoundController.js
@@ -4,31 +4,20 @@ module.exports.handle = (req, res) => {
   const body = req.body
 
   if (!body || !body.text) {
-    res.status(200).send({message: 'success'})
-
-    return PlaySoundService.play()
+    return PlaySoundService.play().then(() => res.status(200).send({message: 'success'}))
   }
   const youtubeUrlRegex = new RegExp(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/)
 
   if (body.text && youtubeUrlRegex.test(body.text)) {
     const slackResponse = {
-      attachments: [
-        {
-          color: '#36a64f',
-          title: 'Playing your link'
-        }
-      ]
+      attachments: [{ color: '#36a64f', title: `Now Playing ${body.text}` }]
     }
-    res.status(200).send(slackResponse)
-    return PlaySoundService.playFromUrl(body.text).catch(e => Promise.reject(new Error(e)))
+    return PlaySoundService.playFromUrl(body.text)
+    .then(() => res.status(200).send(slackResponse))
+    .catch(e => Promise.reject(new Error(e)))
   } else {
     const slackErrorResponse = {
-      attachments: [
-        {
-          color: '#e74c3c',
-          title: 'I need a valid youtube url :('
-        }
-      ]
+      attachments: [{ color: '#e74c3c', title: 'I need a valid youtube url :(' }]
     }
     return res.status(200).send(slackErrorResponse)
   }
